27f5db583b79ae936f85b3beec6ac3e5cab2b721
Vuong <vanvuong2005827@gmail.com>
2025-08-21 11:37:06 +0700

    add: upload api for community service

--- community-portal-service/src/main/java/com/vuong/communityportalservice/service/PostService.java
+++ community-portal-service/src/main/java/com/vuong/communityportalservice/service/PostService.java
@@ -1,7 +1,9 @@
 package com.vuong.communityportalservice.service;
 
 import com.vuong.communityportalservice.dto.PostDto;
+import com.vuong.communityportalservice.dto.UploadFileResponseDto;
 import com.vuong.communityportalservice.exception.CustomException;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -11,5 +13,5 @@ public interface PostService {
     List<PostDto> getAllPosts() throws CustomException;
     void updatePost(PostDto postDto) throws CustomException;
     void deletePost(Long postId) throws CustomException;
-
+    UploadFileResponseDto upload(MultipartFile file) throws CustomException;
 }
